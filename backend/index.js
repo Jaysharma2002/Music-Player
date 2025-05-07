3ef0fcfefa7b73df4fea57b3a7c054c225abfb43
Jaysharma2002 <jaysharmadec2002@gmail.com>
2025-05-07 18:12:35 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 const app=new express()
 app.use(bodyParser.json())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://music-player-1-dpb9.onrender.com',
 }))
 app.post('/login',(req,res)=>{
     const code=req.body.code
@@ -13,7 +13,7 @@ app.post('/login',(req,res)=>{
         return res.status(404).json({message:"No Authorization code"})
     }
     const spotifyapi=new SpotifyWebApi({
-        redirectUri:'http://localhost:5173',
+        redirectUri:'https://music-player-1-dpb9.onrender.com',
         clientId:'0647e19f61a44c20bf6b505514767f63',
         clientSecret:'6d09db906fb647388bda3ee04d2c2dff'
     })
@@ -32,7 +32,7 @@ app.post('/login',(req,res)=>{
 app.post('/refresh',(req,res)=>{
     const refreshToken=req.body.refreshToken
     const spotifyapi=new SpotifyWebApi({
-        redirectUri:'http://localhost:5173',
+        redirectUri:'https://music-player-1-dpb9.onrender.com',
         clientId:'0647e19f61a44c20bf6b505514767f63',
         clientSecret:'6d09db906fb647388bda3ee04d2c2dff',
         refreshToken
